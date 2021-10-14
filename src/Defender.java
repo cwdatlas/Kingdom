bb628d3c6256dd2d983609acb958c7cb502c9a60
adrianrinconj <arincon-jimenez@carroll.edu>
2021-10-14 14:56:10 -0600

    Updated
    Defender

--- src/Defender.java
+++ src/Defender.java
@@ -1,6 +1,9 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 //Programmed by Adrian and Aidan of Carroll college
 public class Defender extends BaseSprite implements DefenderI{
-
+	
 	protected Defender(int X, int Y, String fileName) {
 		super(X, Y, fileName);
 		// TODO Auto-generated constructor stub
@@ -8,13 +11,34 @@ public class Defender extends BaseSprite implements DefenderI{
 
 	@Override
 	public boolean setDefending() {
-		// TODO Auto-generated method stub
-		return false;
+		boolean check = false;
+		try {
+			goToCords[0] = 200;
+			check = true;
+		}
+		finally {
+			
+		}
+		return check;
 	}
 
 	@Override
 	public boolean setRoaming() {
-		// TODO Auto-generated method stub
-		return false;
+		boolean check = false;
+		try {
+			Timer myTimer = new Timer();
+			myTimer.scheduleAtFixedRate(new TimerTask() {
+				public void run() {
+					goToCords[0] = (int)(Math.random() * 100);
+				}
+			}, 4000, 4000);
+			
+			check = true;
+			
+		}
+		finally {
+			
+		}
+		return check;
 	}
 }
