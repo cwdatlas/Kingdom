52f2be427c14627d620fd97dad08cc383045f046
adrianrinconj <arincon-jimenez@carroll.edu>
2021-10-14 15:37:35 -0600

    Fixed Error
    Fixed an error in enemy interface

--- src/EnemyI.java
+++ src/EnemyI.java
@@ -3,7 +3,7 @@ public interface EnemyI {
 
 		void setAttack(PlayableCharacter player); //sets player to be the target of the enemies so the enemies run at the player
 		
-		boolean setRetreat(); //sets the enemy to retreat to their spawn positions
+		void setRetreat(); //sets the enemy to retreat to their spawn positions
 		
 		
 }
