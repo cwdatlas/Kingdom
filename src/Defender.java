547b4729a359b66acd73639e3d25b5d5698146c0
adrianrinconj <arincon-jimenez@carroll.edu>
2021-10-22 14:04:36 -0600

    Defender Update

--- src/Defender.java
+++ src/Defender.java
@@ -1,23 +1,26 @@
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 //Programmed by Adrian and Aidan of Carroll college
 public class Defender extends BaseSprite implements DefenderI {
+	private final Random random;
 
 	protected Defender(int X, int Y, String fileName) {
 		super(X, Y, fileName);
-		// TODO Auto-generated constructor stub
+		random = new Random();
 	}
 
 	@Override
 	public void setDefending() {
-		goToCords[0] = 800;
+		goToCords[0] = random.nextInt(800);
 	}
 
 	@Override
 	public void setRoaming() {
-		if(Math.random() < .004) {
-		goToCords[0] = (int) (Math.random() * 1500);
+		if(Math.random() < .01) {
+//		goToCords[0] = (int) (Math.random() * 1500);
+			moveTo(random.nextInt(1500), goToCords[1] );
 		}
 
 
